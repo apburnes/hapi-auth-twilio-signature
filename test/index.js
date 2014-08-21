a8bce26f484e2ab9ed1f7a5d3352b49047e19b40
Andrew Burnes <apburnes@gmail.com>
2014-08-20 23:42:52 -0700

    initial

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('true', function(){
+  it('should be true', function(){
+    expect(true).to.be.equal(true);
+  });
+});
