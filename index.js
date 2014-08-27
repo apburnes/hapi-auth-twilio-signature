ef956d18867b9bea037f841aca90ab2e99f5b3f3
Andrew Burnes <apburnes@gmail.com>
2014-08-26 22:25:18 -0700

    Auth with tests

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
+'use strict';
 
+var boom = require('boom');
+var hoek = require('hoek');
+
+function scheme(server, options){
+  hoek.assert(options, 'Missing header "X-Twilio-Signature" for authentication.');
+  hoek.assert(typeof options.validateFunc === 'function', 'options.validateFunc must be a function to validate authentication.');
+
+  var twilioSignature = options.twilioSignature || 'x-twilio-signature';
+  var validateFunc = options.validateFunc;
+
+  function authenticate(request, reply){
+    var signature = request.headers[twilioSignature];
+
+    if(!signature){
+      return reply(boom.badRequest('X-Twilio-Signature is not set', 'Twilio-Signature'));
+    }
+
+    validateFunc(signature, function(err, isValid, credentials){
+      credentials = credentials || null;
+
+      if(err){
+        return reply(err, {
+          credentials: credentials,
+          log: {
+            data: err,
+            tags: ['auth', 'Twilio-Signature']
+          }
+        });
+      }
+
+      if(!isValid){
+        return reply(boom.unauthorized('Invalid "X-Twilio-Signature".'), {
+          credentials: credentials,
+          log: {
+            tags: ['auth', 'invalid']
+          }
+        });
+      }
+
+      if(!credentials){
+        return reply(boom.badImplementation('Credentials must be sent to validate authentication.'), {
+          log: {
+            tags: 'credentials'
+          }
+        });
+      }
+
+      return reply(null, {
+        credentials: credentials
+      });
+    });
+  }
+
+  return {
+    authenticate: authenticate
+  };
+}
+
+function register(server, options, next){
+  server.auth.scheme('twilio-signature', scheme);
+  next();
+}
+
+register.attributes = {
+  pkg: require('./package.json')
+};
+
+module.exports = {
+  register: register
+};
--- test/index.js
+++ test/index.js
@@ -3,8 +3,120 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-describe('true', function(){
-  it('should be true', function(){
-    expect(true).to.be.equal(true);
+var hapi = require('hapi');
+var boom = require('boom');
+var extend = require('util')._extend;
+
+var authTwilioSignature = require('../');
+
+describe('Auth Twilio Signature', function(){
+
+  function validateFunc(signature, callback){
+    if (signature === 'validSignature') {
+      var credentials = {
+        twilioSignature: signature
+      };
+
+      return callback(null, true, credentials);
+    }
+    if (signature === 'noCredentials') {
+      return callback(null, true);
+    }
+    if (signature === 'error') {
+      return callback(boom.unauthorized());
+    }
+
+    return callback(null, false);
+  }
+
+  function basicHandler(request, reply){
+    reply('ok');
+  }
+
+  var request;
+  var server = new hapi.Server({debug: false});
+
+  before(function(done){
+    server.pack.register(authTwilioSignature, function(err){
+
+      server.auth.strategy('signature', 'twilio-signature', { validateFunc: validateFunc });
+
+      server.route([
+        { method: 'POST', path: '/auth', handler: basicHandler, config: { auth: 'signature' } }
+      ]);
+
+      done(err);
+    });
+  });
+
+  beforeEach(function(){
+    request = {
+      method: 'POST',
+      url: '/auth',
+    };
+  });
+
+  afterEach(function(){
+    request = null;
+  });
+
+  it('should return a successful 200 auth', function(done){
+    extend(request, {
+      headers: {
+        'X-Twilio-Signature': 'validSignature'
+      }
+    });
+
+    server.inject(request, function(res){
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.be.equal('ok');
+      done();
+    });
+  });
+
+  it('should return a 401 unauthorized with incorrect signature', function(done){
+    extend(request, {
+      headers: {
+        'X-Twilio-Signature': 'notaSignature'
+      }
+    });
+
+    server.inject(request, function(res){
+      expect(res.statusCode).to.equal(401);
+      done();
+    });
+  });
+
+  it('should return a 401 unauthorized when validateFunc sends error', function(done){
+    extend(request, {
+      headers: {
+        'X-Twilio-Signature': 'error'
+      }
+    });
+
+    server.inject(request, function(res){
+      expect(res.statusCode).to.equal(401);
+      done();
+    });
+  });
+
+  it('should return a 500 bad implemtation when credentials are not sent', function(done){
+    extend(request, {
+      headers: {
+        'X-Twilio-Signature': 'noCredentials'
+      }
+    });
+
+    server.inject(request, function(res){
+      expect(res.statusCode).to.equal(500);
+      done();
+    });
+  });
+
+  it('should return a 400 unauthorized with missing signature', function(done){
+    server.inject(request, function(res){
+      expect(res.statusCode).to.equal(400);
+      done();
+    });
   });
 });
